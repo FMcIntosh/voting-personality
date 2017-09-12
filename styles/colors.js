74348eca6e8b32035d4bc70e6baea42b69dd6007
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-12 22:05:28 +1200

    Add global styles folder

---
+++ styles/colors.js
@@ -0,0 +1,9 @@
+export const
+    orange = "#ff984a",
+    darkOrange = "#ff6437",
+    // green = "#1db954",
+    green = "#1ed760",
+    darkGrey = "#3c3c3e", 
+    lightGrey ="#f8f8f8",
+    midGrey = "#4e4e4e",
+    black= "#000"
