4c935d7771e13c83df43f4925bf36a67a47cfc5d
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-22 23:46:21 +1200

    Use material-ui pattern for colours

--- styles/colors.js
+++ styles/colors.js
@@ -1,10 +1,20 @@
 export const
-    orange = "#ff984a",
+    // orange = "#ff984a",
     // orange = "#F55A3F",
-    darkOrange = "#ff6437",
+    // darkOrange = "#ff6437",
     // green = "#1db954",
-    green = "#1ed760",
-    darkGrey = "#3c3c3e", 
-    lightGrey ="#f8f8f8",
-    midGrey = "#4e4e4e",
-    black= "#000"
+    // green = "#1ed760",
+    // darkGrey = "#3c3c3e", 
+    // lightGrey ="#f8f8f8",
+    // midGrey = "#4e4e4e",
+    // black= "#000"
+    primary="#ff9800",
+    darkPrimary="#f57c00",
+    lightPrimary="#ffe0b2",
+    icon="#212121",
+    accent="#7c4dff",
+    textPrimary="#212121",
+    textSecondary="#757575",
+    divider="#BDBDBD"
+
+    
