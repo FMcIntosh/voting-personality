3afa9cff5c7937e5c6a979d82377bcee73701e6f
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-23 18:28:39 +1200

    Create HOC to provide MuiTheme to pages

---
+++ hoc/MuiThemeWrapper/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import indigo from 'material-ui/colors/indigo';
+import orange from 'material-ui/colors/orange';
+import red from 'material-ui/colors/red';
+
+const decorator = (ComposedComponent) => {
+  
+  const theme = createMuiTheme({
+    palette: {
+      primary: orange, // Purple and green play nicely together.
+      secondary: {
+        ...indigo,
+        A200: '#536dfe',
+      },
+      error: red,
+    },
+  });
+  
+  return class extends Component {
+
+    static async getInitialProps(ctx) {
+      const { req } = ctx;
+      const isServer = !!req;
+      const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+
+      let pageProps = {};
+
+      if (ComposedComponent.getInitialProps) {
+        pageProps = await ComposedComponent.getInitialProps(ctx);
+      }
+
+      return {
+        ...pageProps,
+        isServer,
+        userAgent,
+      };
+    }
+
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+      return (
+        <div>
+          <MuiThemeProvider theme={theme}>
+              <ComposedComponent
+                {...this.props}
+              />
+            </MuiThemeProvider>
+        </div>
+      )
+    }
+  };
+};
+
+export default decorator;
