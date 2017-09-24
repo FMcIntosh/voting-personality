ecca172c10b9aa42ad04cc8d03663fc9a68df1bb
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-24 18:36:05 +1300

    Create component implemented with mui

---
+++ components/AppBar/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import { withTheme } from 'material-ui/styles';
+
+const MyAppBar =  ({ theme, children }) => {
+  const lightPrimaryText = theme.palette.primary[100];
+  const primaryColor = theme.palette.primary[500];
+  
+  console.log(theme.palette)
+
+  const styles = {
+    primaryText: {
+      padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+      color: lightPrimaryText,
+    }
+  }
+
+  return (
+  <AppBar position="fixed" color="primary">
+    <Toolbar>
+      <Typography type="title" style={styles.primaryText}>
+        {children}
+      </Typography>
+    </Toolbar>
+  </AppBar>
+  )
+}
+
+export default withTheme(MyAppBar);
