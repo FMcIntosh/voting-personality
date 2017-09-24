0f6df9a05041377e87d40c35807e52dea82b1834
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-24 18:41:24 +1300

    Use mui components

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-
-import Header from 'components/Header';
-import Card from 'components/Card';
-import CardTitle from 'components/CardTitle';
+import { Title, Paragraph } from 'components/Text';
 import Button from 'components/Button';
-
-import { green, darkOrange, darkGrey } from 'styles/colors';
-import Link from 'next/link';
-
 import { primary, lightPrimary } from 'styles/colors';
+import MuiThemeWrapper from 'hoc/MuiThemeWrapper';
 
-export default () => (
+const text1 = "Ever wondered if your personality influences who you vote for? Research into "
++ "the link between personality and voting has shown that there is correlation between big five personality"
++ "and who you vote for";
+
+const text2 = "Here's your chance to see if your personality might have influenced who you voted for in New Zealand's 2017 general election"
+
+
+const Index = () => (
   <Background>
     <Wrapper>
-      <CardTitle>Voting and Personality</CardTitle>
-      <Paragraph>Research into the link between personality and voting has shown that there is correlation between your big five personality
-                      and so on.
-      </Paragraph> 
-      <Paragraph>Here's your chance to see if your personality might influence who you voted for in New Zealand's 2017 general election.
-      </Paragraph>        
+      <Title light>Guess My Vote</Title>
+      <Paragraph light>{text1}</Paragraph> 
+      <Paragraph light>{text2}</Paragraph>        
       <Button href="/personality">Get Started!</Button>
     </Wrapper>
   </Background>
 ) 
+
+export default MuiThemeWrapper(Index);
 
 const Background = styled.div`
     width: 100vw;
@@ -34,14 +34,10 @@ const Background = styled.div`
     align-items: center;
     background: ${primary};
 `
-const Paragraph = styled.p`
-  color: white;
-  font-weight: 300;
-  text-align: center;
-`
 const Wrapper = styled.div`
   width: 60%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-top: 50px
 `
