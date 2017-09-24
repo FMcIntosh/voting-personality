0b2a88508f84c27ef45ae4a2fb346ac1889b5eab
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-24 18:35:20 +1300

    Create Text components implemented using mui typography

---
+++ components/Text/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import { darkGrey } from 'styles/colors';
+import { withTheme } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+
+// const getTheme = ({ theme }) => theme;
+// const theme = withTheme(getTheme)();
+
+
+export const Title = withTheme(({ theme, children, light }) => {
+  const style = {
+    color: light ? "white" : theme.palette.text.primary,
+    fontSize: "48px",
+    lineHeight: "48px",
+    fontWeight: "400"
+  }
+
+  return (
+    <Typography type="title" align="center" gutterBottom style={style} color="default">{children}</Typography>
+  )
+})
+
+export const Header = withTheme(({ theme, children, light }) => {
+  const style = {
+    color: light ? "white" : theme.palette.text.primary,
+    fontSize: "40px",
+    lineHeight: "48px",
+    fontWeight: "300"
+  }
+
+  return (
+    <Typography type="title" align="center" gutterBottom style={style} color="default">{children}</Typography>
+  )
+})
+
+export const Paragraph = withTheme(({ theme, children, light }) => {
+  const style = {
+    color: light ? theme.palette.primary[50] : theme.palette.text.primary,
+    fontSize: "20px",
+    fontWeight: "300",
+    margin: "20px"
+  }
+
+  return (
+    <Typography type="body1" align="center" gutterBottom style={style} >{children}</Typography>
+  )
+})
+
+
