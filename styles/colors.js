a621d6783302de2a23c524eca19d4bc13cdd96ce
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-26 11:33:35 +1300

    Change styles format to match mui

--- styles/colors.js
+++ styles/colors.js
@@ -1,13 +1,4 @@
 export const
-    // orange = "#ff984a",
-    // orange = "#F55A3F",
-    // darkOrange = "#ff6437",
-    // green = "#1db954",
-    // green = "#1ed760",
-    // darkGrey = "#3c3c3e", 
-    // lightGrey ="#f8f8f8",
-    // midGrey = "#4e4e4e",
-    // black= "#000"
     primary="#ff9800",
     darkPrimary="#f57c00",
     lightPrimary="#ffe0b2",
