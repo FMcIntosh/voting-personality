803140291f5fc2b651c65df40c5f843d8b10099a
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-26 11:36:43 +1300

    Add custom style to input to reduce margin

--- components/PersonalitySection/Input.js
+++ components/PersonalitySection/Input.js
@@ -6,12 +6,18 @@ import { withStyles } from 'material-ui/styles';
 import TextField from 'material-ui/TextField';
 
 
-export default ({ placeholder, id}) => (
-  <TextField
-    label={placeholder}
-    placeholder="/100"
-    key={id}
-    margin="normal"
-  />
-)
+export default ({ placeholder, id}) => {
+  const styles = {
+   marginTop: "10px"
+  }
+  return (
+    <TextField
+      label={placeholder}
+      placeholder="/100"
+      key={id}
+      margin="dense"
+      styles={styles}
+    />
+  )
+}
 
