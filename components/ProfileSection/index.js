c034190e220a3dccd468cf987a325fffa5e4c113
Fraser McIntosh <frasermcintosh1@gmail.com>
2017-09-26 11:32:51 +1300

    Create section for user profile information

---
+++ components/ProfileSection/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Input, { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import Grid from 'material-ui/Grid';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+import Select from 'material-ui/Select';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+});
+
+class SimpleSelect extends React.Component {
+  state = {
+    age: '',
+    qualification: '',
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  render() {
+    const classes = this.props.classes;
+
+    return (
+      <form className={classes.container} autoComplete="off">
+        <Grid container direction="column" lg="12" sm="12">
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="gender">Gender</InputLabel>
+          <Select
+            value={this.state.age}
+            onChange={this.handleChange('age')}
+            input={<Input id="gender" />}
+          >
+            <MenuItem value="male">Male</MenuItem>
+            <MenuItem value="female">Female</MenuItem>
+            <MenuItem value="other">Other</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="qualification">Qualification</InputLabel>
+          <Select
+            value={this.state.age}
+            onChange={this.handleChange('qualification')}
+            input={<Input id="qualification" />}
+          >
+            <MenuItem value="highschool">High School</MenuItem>
+            <MenuItem value="university">University</MenuItem>
+            <MenuItem value="other">Other</MenuItem>
+          </Select>
+          <FormHelperText>Highest education level</FormHelperText>
+        </FormControl>
+        </Grid>
+      </form>
+    );
+  }
+}
+
+SimpleSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleSelect);
